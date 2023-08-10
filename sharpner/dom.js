ac8bb20b17ddf662a757fbbe0800af9d40081f92
rohit3132 <rohitlaskar95@gmail.com>
2023-08-10 15:10:02 +0530

    dom-js

---
+++ sharpner/dom.js
@@ -0,0 +1,24 @@
+//EXAMINE THE DOCUMENT OBJECT //
+
+//console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10]
+
+var headerTitle = (document.getElementById('header-title'));
+var header=document.getElementById('main-header');
+// //headerTitle.innerHTML = '<h3>Hello</h3>';
+ headerTitle.style.borderBottom ='solid 3px #000 ';
+ var title = document.getElementById('title');
+ title.style.color ='green';
+ title.style.fontWeight='900';
+ items[1].textContext='Hello 2';
+ items[1].style.fontWeight='bold';
+ items[1].style.backgrpund='yellow';
