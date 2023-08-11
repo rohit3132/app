062e2638fa9cfbfd54b56d2ec52c4e7a92492c89
rohit3132 <rohitlaskar95@gmail.com>
2023-08-11 13:56:32 +0530

    dom-class-edit

--- sharpner/dom.js
+++ sharpner/dom.js
@@ -12,13 +12,28 @@
 // console.log(document.all[10]);
 // document.all[10]
 
-var headerTitle = (document.getElementById('header-title'));
-var header=document.getElementById('main-header');
-// //headerTitle.innerHTML = '<h3>Hello</h3>';
- headerTitle.style.borderBottom ='solid 3px #000 ';
- var title = document.getElementById('title');
- title.style.color ='green';
- title.style.fontWeight='900';
- items[1].textContext='Hello 2';
- items[1].style.fontWeight='bold';
- items[1].style.backgrpund='yellow';
+// var headerTitle = (document.getElementById('header-title'));
+// var header=document.getElementById('main-header');
+// // //headerTitle.innerHTML = '<h3>Hello</h3>';
+//  headerTitle.style.borderBottom ='solid 3px #000 ';
+//  var title = document.getElementById('title');
+//  title.style.color ='green';
+//  title.style.fontWeight='900';
+//  items[1].textContext='Hello 2';
+//  items[1].style.fontWeight='bold';
+//  items[1].style.backgrpund='yellow';
+
+//  items.style.backgroundColor = '#f4f4f4';
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+
+items[2].style.background = 'green';
+// Gives Error
+// items.style.background =  '#f4f4f4';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+
+}
+
